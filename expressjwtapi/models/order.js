b7cd471f0848f049471c84e390e5ccb9be4de21e
Emir Sinan İşler <127344504+emirsinann@users.noreply.github.com>
2023-09-30 16:47:17 +0300

    backend files

---
+++ expressjwtapi/models/order.js
@@ -0,0 +1,26 @@
+
+const {DataTypes } = require("sequelize");
+const { sequelise } = require("../dbconfig");
+
+const Order = sequelise.define('orders', {
+    orderID : {
+        primaryKey : true,
+        autoIncrement : true,
+        type:DataTypes.INTEGER
+    },
+    iscredit:{
+       type:  DataTypes.BOOLEAN,
+       allowNull : false
+    },
+    totalPrice: DataTypes.INTEGER,
+    siparisTarihi : DataTypes.DATE,
+    orderNote : DataTypes.STRING,
+    
+  },
+  {
+    timestamps : false
+  })
+  //async()
+  //await order.sync({ force: true });
+  //console.log("The table for the Order model was just (re)created!");
+exports.Order =Order
